8cd1b43c496ccbf3fd0abd0677efad467e47277e
Michael Chapman <submariner100@gmail.com>
2016-11-24 17:31:40 +0000

    The UI interface has been added, also the Pokemon class file

---
+++ PokeGotcha/Pokemon.swift
@@ -0,0 +1,34 @@
+//
+//  Pokemon.swift
+//  PokeGotcha
+//
+//  Created by Macbook on 24/11/2016.
+//  Copyright © 2016 Chappy-App. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+     
+     private var _name: String!
+     private var _pokedexId: Int!
+     
+     var name: String {
+          
+          return _name
+     }
+     
+     var pokedexId: Int {
+          
+          return _pokedexId
+          
+     }
+     
+     init(name: String, pokedexId: Int) {
+          
+          self._name = name
+          self._pokedexId = pokedexId
+     }
+     
+      
+}
