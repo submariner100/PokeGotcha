6094d1fad3b075309fa0ae3f914245c4ee7cca7f
Michael Chapman <submariner100@gmail.com>
2016-11-25 19:59:59 +0000

    Added audio and custom fonts to project

--- PokeGotcha/ViewController.swift
+++ PokeGotcha/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
      @IBOutlet weak var collection: UICollectionView!
      
      var pokemon = [Pokemon]()
+     var musicPlayer: AVAudioPlayer!
+     
      
 
      override func viewDidLoad() {
@@ -21,7 +24,31 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
           collection.delegate = self
           collection.dataSource = self
           parsePokemonCSV()
+          initAudio()
      }
+     
+     func initAudio(){
+          
+          let path = Bundle.main.path(forResource: "music", ofType: "mp3")
+          
+          do {
+               musicPlayer = try AVAudioPlayer(contentsOf: URL(string: path!)!)
+               musicPlayer.prepareToPlay()
+               musicPlayer.numberOfLoops = -1
+               musicPlayer.play()
+               
+               
+               
+          } catch let err as NSError {
+               
+               print(err.debugDescription)
+          }
+     }
+     
+     
+     
+     
+     
      
      func parsePokemonCSV() {
      
@@ -88,6 +115,18 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
           
      }
      
+     @IBAction func musicBtnPressed(_ sender: UIButton) {
+     
+          if musicPlayer.isPlaying {
+               
+               musicPlayer.pause()
+               sender.alpha = 0.2
+          } else {
+               
+               musicPlayer.play()
+               sender.alpha = 1.0
+          }
+     }
 
 }
 
